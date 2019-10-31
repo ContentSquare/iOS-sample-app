157ab362591dd64997701562d89cfcbfab98dd0b
Téo Metz <teo.metz@contentsquare.com>
2019-10-31 15:04:19 +0100

    Make the app compatible with iOS 12

--- iOS-sample-app/AppDelegate.swift
+++ iOS-sample-app/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Note that you do not need to manually start the Contentsquare SDK yourself, it
