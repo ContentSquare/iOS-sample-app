49b8dfd4d549d57485991eace1caeef7a117c0ee
Téo Metz <teo.metz@contentsquare.com>
2019-11-08 16:34:08 +0100

    Be more assertive in instructing to put screen views in view did appear

--- iOS-sample-app/Showcases/Screen Views/DefaultScreenViewController.swift
+++ iOS-sample-app/Showcases/Screen Views/DefaultScreenViewController.swift
@@ -4,15 +4,13 @@ import ContentSquare
 
 class DefaultScreenViewController: UIViewController {
 
-    // In most cases, the simplest place to track screen views is in the controller's
-    // viewDidAppear.
+    // The right place to track screen views is in the controller's viewDidAppear.
     //
-    // Avoid putting screen views in viewWillAppear. It is advised to avoid puting non critical
-    // work in viewWillAppear, since you don't want to use the main thread unnecessarily while
-    // a view is in the process of being displayed.
+    // Don't put screen views in viewWillAppear. You should not put non critical work in viewWillAppear, since you don't
+    // want to use the main thread unnecessarily while a view is in the process of being displayed.
     //
-    // Also, avoid using viewDidLoad, since it won't always be called. For instance, it won't
-    // be called in cases of back navigation in a navigation stack.
+    // Also, don't use viewDidLoad, since it won't always be called. For instance, it won't be called in cases of back
+    // navigation in a navigation stack.
     override func viewDidAppear(_ animated: Bool) {
         // Remember to always call super in a viewDidAppear
         super.viewDidAppear(animated)
