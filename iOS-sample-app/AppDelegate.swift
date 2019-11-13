7cada6ac650b73007a7e440a666d319529df1ee1
Téo Metz <teo.metz@contentsquare.com>
2019-11-13 17:34:40 +0100

    Wording

--- iOS-sample-app/AppDelegate.swift
+++ iOS-sample-app/AppDelegate.swift
@@ -6,8 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Note that you do not need to manually start the Contentsquare SDK yourself, it
-        // autostarts by itself
+        // Note that you should not manually start the Contentsquare SDK, it autostarts by itself
         return true
     }
 }
