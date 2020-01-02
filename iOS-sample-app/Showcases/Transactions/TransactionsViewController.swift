85db605b049e563846d77748a5b2a18ecc9f0bf3
Xavier Coutin <xavier.coutin@contentsquare.com>
2020-01-02 15:14:10 +0100

    [mobile-1905] Send one Transaction for one purchase, not one for each product

--- iOS-sample-app/Showcases/Transactions/TransactionsViewController.swift
+++ iOS-sample-app/Showcases/Transactions/TransactionsViewController.swift
@@ -16,19 +16,20 @@ struct Item
 }
 
 // This class showcases how to track purchases made by an user with CustomerTransactions
-// Add some items to the cart, then validate it to send the transactions
+// Add some items to the cart, then validate it to send the transaction
 class TransactionsViewController: UIViewController {
 
     private var cart = [Item]()
     private var selectedCurrency : Currency = .eur
     private var itemCount = 0
+    private var transactionCount = 0
 
     // MARK: - Buttons actions
 
     @IBAction private func addItemToCart(_ sender: UIButton)
     {
         let itemTitle = "item \(itemCount)"
-        let itemPrice = Float(itemCount)
+        let itemPrice = Float(10)
         let newItem = Item(title: itemTitle, price: itemPrice, currency: selectedCurrency)
         cart.append(newItem)
         itemCount += 1
@@ -38,10 +39,13 @@ class TransactionsViewController: UIViewController {
     {
         // As a transaction represents a purchase made by a customer, you don't send the transaction as soon as the user adds an item to the cart
         // Wait for the user to validate the purchase
-        for item in cart
-        {
-            ContentSquare.send(transaction: CustomerTransaction(id: item.title, value: item.price, currency: item.currency))
+        var transactionValue: Float = 0
+        cart.forEach { item in
+            transactionValue += item.price
         }
+        ContentSquare.send(transaction: CustomerTransaction(id: "Transaction\(transactionCount)", value: transactionValue, currency: selectedCurrency))
+        transactionCount += 1
+
         cart.removeAll()
         itemCount = 0
     }
