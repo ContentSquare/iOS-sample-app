8c9b02d935d45018c8248d9a7fb3a109319be19f
Xavier Coutin <xavier.coutin@contentsquare.com>
2020-01-07 11:03:13 +0100

    [mobile-1906] Add a sample demonstrating how to use dynamic variables (#5)
    * [mobile-1906] Add a sample demonstrating how to use dynamic variables
    
    * [mobile-1906] Better explanations and better error handling
    
    * [mobile-1906] Use storyboard instead of coding the UI
    
    * [mobile-1906] Better use of dynamic variables
    
    * [mobile-1906] More agnostic UI example
    
    * [mobile-1906] Update storyboard
    
    * [mobile-1906] Minor improvements

---
+++ iOS-sample-app/Showcases/DynamicVariables/DynamicVariablesViewController.swift
@@ -0,0 +1,78 @@
+import UIKit
+import ContentSquare
+
+enum AccountType: Int {
+    case standard = 0
+    case premium = 1
+}
+
+// This class showcases how to use dynamic variables to gather additional data about the session,
+// such as which version of an A/B test the user saw, or other relevant user data
+class DynamicVariablesViewController: UIViewController {
+
+    @IBOutlet private weak var testButton: UIButton!
+    private var testButtonColor = UIColor()
+    private var userAge: Int = 0
+    private var userAccount: AccountType = .standard
+
+    override func loadView()
+    {
+        super.loadView()
+
+        // Get the color for the button from A/B test configuration
+        // For instance, yellow or green
+        testButtonColor = .yellow
+        testButton.backgroundColor = testButtonColor
+        // Retrieve the user's age and his account type, from his personal account for instance
+        userAge = 24
+        userAccount = .premium
+
+        sendDynamicVariables()
+    }
+
+    //MARK: - Dynamic vars
+
+    private func sendDynamicVariables()
+    {
+        // Are users more likely to tap on a yellow or a green button?
+        let colorValue = testButtonColor.isEqual(UIColor.yellow) ? "yellow" : "green"
+        sendDynamicVariable(key: "Button Color", value: colorValue)
+        // Also send the user's age and his account type, to determine if it has an impact on the A/B test
+        sendDynamicVariable(key: "User age", value: userAge)
+        let accountValue = userAccount == .standard ? "standard" : "premium"
+        sendDynamicVariable(key: "User account", value: accountValue)
+    }
+
+    //MARK: - Helpers
+
+    private func sendDynamicVariable(key: String, value: String)
+    {
+        do {
+            let dynamic = try DynamicVar(key: key, value: value)
+            ContentSquare.send(dynamicVar: dynamic)
+        }
+        catch {
+            sendLog(message: "Could not create Contentsquare String DynamicVar: \(error)")
+        }
+    }
+
+    private func sendDynamicVariable(key: String, value: Int)
+    {
+        do {
+            let dynamic = try DynamicVar(key: key, value: value)
+            ContentSquare.send(dynamicVar: dynamic)
+        }
+        catch {
+            sendLog(message: "Could not create Contentsquare Int DynamicVar: \(error)")
+        }
+    }
+
+    //MARK: - Error handling
+
+    private func sendLog(message: String)
+    {
+        // The DynamicVar constructor can throw errors if it considers your key or value invalid.
+        // Take a look at its Xcode documentation to know what is an invalid key or value. If you are sure your values are valid, you can ignore errors.
+        // But you might probably prefer sending yourself a log, to be aware if your dynamic variables get refused, especially if validation criterias change in the future.
+    }
+}
