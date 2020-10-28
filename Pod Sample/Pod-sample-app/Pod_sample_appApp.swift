acd0ed33704c97ba0ab040ef10508afe34027efb
mathieu <mathieu.ouvrard@contentsquare.com>
2020-10-28 14:38:12 +0100

    release/4.0.0 (#12)
    * Move project to Carthage Sample folder
    
    * Add Pod Sample project
    
    * carthage update

---
+++ Pod Sample/Pod-sample-app/Pod_sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pod_sample_appApp.swift
+//  Pod-sample-app
+//
+//  Created by Mathieu Ouvrard on 28/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Pod_sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
