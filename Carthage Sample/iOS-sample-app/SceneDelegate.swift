a8f36a0f931d53d5642c5083670cbd5435e1eb0b
Xavier Coutin <xavier.coutin@contentsquare.com>
2020-11-18 09:42:22 +0100

    Handle url schemes for cs-in-app feature

--- Carthage Sample/iOS-sample-app/SceneDelegate.swift
+++ Carthage Sample/iOS-sample-app/SceneDelegate.swift
@@ -1,7 +1,14 @@
-
 import UIKit
+import ContentsquareModule
 
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        // Handle incoming urls to monitor CS in-app activation
+        if let url = URLContexts.first?.url {
+            Contentsquare.handle(url: url)
+        }
+    }
 }
