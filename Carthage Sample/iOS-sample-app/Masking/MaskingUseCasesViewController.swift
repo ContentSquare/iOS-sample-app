0a59f80aa70b3515c198c66b07a7e9ffad883e10
Xavier Coutin <xavier.coutin@contentsquare.com>
2021-04-01 17:43:49 +0200

    Code review feedbacks

--- Carthage Sample/iOS-sample-app/Masking/MaskingUseCasesViewController.swift
+++ Carthage Sample/iOS-sample-app/Masking/MaskingUseCasesViewController.swift
@@ -7,15 +7,13 @@ class MaskingUseCasesViewController: UIViewController {
     override func loadView() {
         super.loadView()
 
-        maskingTableView.delegate = self
-        maskingTableView.dataSource = self
         maskingTableView.register(UITableViewCell.self, forCellReuseIdentifier: "maskingIdentifier")
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "maskingSegue",
            let indexPath = maskingTableView.indexPathForSelectedRow {
-            let useCase = allUseCases[indexPath.item]
+            let useCase = MaskingUseCase.allCases[indexPath.item]
             if let maskingVC = segue.destination as? MaskingViewController {
                 maskingVC.useCase = useCase
             }
@@ -23,29 +21,22 @@ class MaskingUseCasesViewController: UIViewController {
     }
 }
 
-enum MaskingUseCase: String {
+enum MaskingUseCase: String, CaseIterable {
     case defaultMasking
     case fullyUnmasked
     case maskImages
     case unmaskPublicLabel
 }
 
-fileprivate let allUseCases: [MaskingUseCase] = [
-    .defaultMasking,
-    .fullyUnmasked,
-    .maskImages,
-    .unmaskPublicLabel
-]
-
 extension MaskingUseCasesViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allUseCases.count
+        return MaskingUseCase.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "maskingIdentifier", for: indexPath)
-        cell.textLabel?.text = "\(allUseCases[indexPath.item].rawValue.localizedCapitalized)"
+        cell.textLabel?.text = "\(MaskingUseCase.allCases[indexPath.item].rawValue.localizedCapitalized)"
         return cell
     }
 }
