03d7d48fe172b6a2cb2b973b856bb685edbc9d06
Mathieu Ouvrard <mathieu.ouvrard@contentsquare.com>
2022-07-26 17:28:00 +0200

    Pass logLevel to `info` for all samples

--- SPM Sample/SPM-sample-app/AppDelegate.swift
+++ SPM Sample/SPM-sample-app/AppDelegate.swift
@@ -1,10 +1,12 @@
+
 import UIKit
+import ContentsquareModule
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Note that you should not manually start the Contentsquare SDK, it autostarts by itself
+        Contentsquare.logLevel = .info
         return true
     }
 }
