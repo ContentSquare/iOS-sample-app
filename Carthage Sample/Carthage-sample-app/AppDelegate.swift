b22f7c96c4bd77396c88a3f287f5bd2c3f2bb655
Mathieu Ouvrard <mathieu.ouvrard@contentsquare.com>
2022-08-01 16:16:06 +0200

    Release iOS 4.15.1 (#32)

--- Carthage Sample/Carthage-sample-app/AppDelegate.swift
+++ Carthage Sample/Carthage-sample-app/AppDelegate.swift
@@ -1,12 +1,13 @@
 
 import UIKit
+import ContentsquareModule
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Note that you should not manually start the Contentsquare SDK, it autostarts by itself
+        Contentsquare.logLevel = .info        
         return true
     }
 }
