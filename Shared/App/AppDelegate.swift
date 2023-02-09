185ecf923f323ab7ed1cb75720bed72c08f5cc14
WANG Jie <jacques09125715@gmail.com>
2023-02-09 11:37:52 +0100

    Add request details for api errors

--- Shared/App/AppDelegate.swift
+++ Shared/App/AppDelegate.swift
@@ -1,13 +1,17 @@
 
 import UIKit
 import ContentsquareModule
+import ContentsquareErrorAnalysisModule
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Contentsquare.logLevel = .info        
+        Contentsquare.logLevel = .info
+        ErrorAnalysis.setURLMaskingPatterns([
+            "https://httpstat.us/:status_code/person/:person_id/store/:store_id"
+        ])
         return true
     }
 }
